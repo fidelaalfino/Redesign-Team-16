82463ac2724f0adcdbcf414e1fdd9e1febf895ed
Hikmah Maharani <hmaharani19@gmail.com>
2025-08-17 12:54:59 +0700

    Update server.js

--- Back-end/server.js
+++ Back-end/server.js
@@ -15,11 +15,10 @@ app.use(express.json());
 
 // Mengambil konfigurasi dari Environment Variables
 const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT || 3306 // Gunakan port dari env, atau default 3306
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_fasilkom_unsri'
 };
 
 // 5. Rute API
